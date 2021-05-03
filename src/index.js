f28349e631638d99dc2746588535fc7743ddec89
Angel Mgl <angelemegeele@gmail.com>
2021-05-03 17:16:42 -0400

    refactor: Update the baseURl of axios

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import "./css/styles.css";
 
 const token = localStorage.getItem("token") || null;
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = "https://jopy.herokuapp.com/apin";
 if(token) axios.defaults.headers.common["x-access-token"] = token;
 
 ReactDOM.render(
